f3a29fcef676cc85a26b65108b5bfd12846727af
JakeBrose <mordecaistudios@gmail.com>
2018-03-01 07:06:29 -0600

    refactored using code shown in class

--- cheer.js
+++ cheer.js
@@ -10,5 +10,23 @@ const cheer = ({ firstName: first, lastName: second }) => {
     console.log('myName=',myName);
     let nameArr = [...first, ...second];
     console.log('nameArr=',nameArr);
+    let i = nameArr.length
+    let j = 0
+    function cheerLetters(i) {
+        if (i) {
+            let letter = nameArr[j].toUpperCase();
+            let conj = "aeiofhlmnrsx".includes(letter.toLowerCase())
+            ? "an"
+            : "a ";
+            console.log(`Gimmie ${conj} ${letter}!`);
+            i--;
+            j++;
+            cheerLetters(i);
+        } else {
+            console.log(`What's that spell?`);
+            console.log(myName);
+        }
+    }
+    cheerLetters(i)
 }
 cheer(fullName)
